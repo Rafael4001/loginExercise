44ad355982017f0bdfe1172ff8e0b43dc4697c23
Rafał Ciesielczuk <rafal.ciesielczuk@gmail.com>
2020-04-26 22:38:37 +0200

    step

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Formik, Field, Form } from 'formik';
-
 import { validationSchemaYup } from './LoginPageValidation'
+
+import Button from '@material-ui/core/Button';
 
 import { loginUser } from "../../services/loginService";
 
 import { useStyles } from './LoginPage.style'
-import { boolean } from "yup";
 
+import { isEmpty } from '../../utilities'
 
 const PASSWORD_TEXT = "Hasło";
 const EMAIL_TEXT = "Email";
@@ -32,33 +33,41 @@ const LoginPage: React.FC<{}> = () => {
 
   const handleSubmit = async (values: FormValues) => {
     const results = await loginUser();
-    console.log('result tutaj', results)
-
 
     //this is made for the task
-    if (results && results[0]?.registered?.age > 10) {
-      await console.log(results[0]?.registered)
-      setIsLoggedIn(true)
-      console.log('results[0]?.email', results[0]?.email)
+
+    if (results) {
       setUser(results[0]?.email)
 
-    } else if (results && results[0]?.registered?.age) {
-      setIsLoggedIn(false)
-      setUser(results[0]?.email)
+      if (results[0]?.registered?.age > 10) {
+        console.log(`wiek ${results[0].registered?.age} jest wystarczajacy`, )
+        setIsLoggedIn(true)
+      } else if (results[0]?.registered?.age) {
+        console.log(`wiek ${results[0].registered?.age} to za mało !`, )
+        setIsLoggedIn(false)
+      }
+
     }
+
+
   };
 
 
+  const logOut = () => {
+    setIsLoggedIn(false);
+    setUser(null);
+
+  }
   const getActualPage = () => (
     <div>
       Witaj <strong>{user}</strong>
-      <button onClick={() => setIsLoggedIn(false)}>Wyloguj</button>
+      <button onClick={logOut}>Wyloguj</button>
     </div>
   )
 
   const getLoginPage = () => (
     <div>
-      Zaloguj się
+      <span>Panel Logowania</span>
 
       <Formik
         initialValues={initialValues}
@@ -73,36 +82,43 @@ const LoginPage: React.FC<{}> = () => {
             handleSubmit,
             isSubmitting,
             touched,
-          }) => (
-          <Form onSubmit={handleSubmit}>
-            <div>
-              <label>{EMAIL_TEXT}</label>
-              <Field
-                type="email"
-                name="email"
-                className={errors.email && touched.email && classes.error}
-              />
-              {touched.email && errors.email && <div>{errors.email}</div>}
-            </div>
+          }) => {
+          const isError = !isEmpty(errors);
 
-            <div>
-              <label>{PASSWORD_TEXT}</label>
-              <Field
-                type="password"
-                name="password"
-                className={errors.password && touched.password && classes.error}
-              />
-              {touched.password && errors.password && <div>{errors.password}</div>}
-            </div>
+          return (
+            <Form onSubmit={handleSubmit}>
+              <div>
+                <label>{EMAIL_TEXT}</label>
+                <Field
+                  type="email"
+                  name="email"
+                  className={errors.email && touched.email && classes.error}
+                />
+                {touched.email && errors.email && <div>{errors.email}</div>}
+              </div>
 
-            <button
-              type={"submit"}
-              disabled={isSubmitting}
-            >
-              {LOGIN_TEXT}
-            </button>
-          </Form>
-        )}
+              <div>
+                <label>{PASSWORD_TEXT}</label>
+                <Field
+                  type="password"
+                  name="password"
+                  className={errors.password && touched.password && classes.error}
+                />
+                {touched.password && errors.password && <div>{errors.password}</div>}
+              </div>
+
+              <Button
+                variant="outlined"
+                color="primary"
+                type={"submit"}
+                disabled={isError || isSubmitting}
+              >
+                {LOGIN_TEXT}
+              </Button>
+            </Form>
+
+          )
+        }}
       </Formik>
 
       {user && <div>Uzytkownik <strong>{user}</strong> nie istnieje</div>}
